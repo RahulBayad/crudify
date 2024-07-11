f7193612e51927c71651637c0c3a5f74a87ab502
RahulBayad <rahulbayad05@gmail.com>
2024-07-11 11:00:18 +0530

    adjustments

--- index.js
+++ index.js
@@ -1,26 +1,27 @@
 const express = require('express');
 const app = express();
-    const path = require('path');
+const path = require('path');
     
-    app.use(express.urlencoded({ extended: true }));
-    app.use(express.json());
-    app.use(express.static(__dirname + '/views'));
-    app.set('view engine','ejs');
-    app.get('/' , (req,res)=>{
-        res.render('index');
-    })
-    app.get('/index', (req, res) => {
-        res.render('index');
-    });
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(__dirname + '/views'));
+app.set('view engine','ejs');
+app.get('/' , (req,res)=>{
+    res.render('index');
+})
+app.get('/index', (req, res) => {
+    res.render('index');
+});
 
-    const mongoose = require('mongoose');
-    mongoose.connect('mongodb://localhost:27017/e-comm');
-    const productSchema = new mongoose.Schema({
-        item:String,
-        type:String,
-        price:Number    
-    })
-    const Product = mongoose.model('products',productSchema);
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/e-comm');
+const productSchema = new mongoose.Schema({
+    item:String,
+    type:String,
+    price:Number    
+})
+    
+const Product = mongoose.model('products',productSchema);
 
     app.post('/insertData',async (req, res) => {
         console.log(req.body);
